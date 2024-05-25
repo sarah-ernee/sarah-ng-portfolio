182c988336b59913ea84a6b02417d5b6a1fd0ad0
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-05-25 17:40:24 +0800

    fixed bootstrap navbar issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -23,10 +29,10 @@ export const NavBar = () => {
   };
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="#home">
-          <img src={""} alt="Logo" />
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -34,7 +40,7 @@ export const NavBar = () => {
         </Navbar.Toggle>
 
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link
               href="#home"
               className={
@@ -66,14 +72,14 @@ export const NavBar = () => {
 
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#">
-                <img src={""} alt="" />
+              <a href="/">
+                <img src={navIcon1} alt="" />
               </a>
-              <a href="#">
-                <img src={""} alt="" />
+              <a href="/">
+                <img src={navIcon2} alt="" />
               </a>
-              <a href="#">
-                <img src={""} alt="" />
+              <a href="/">
+                <img src={navIcon3} alt="" />
               </a>
             </div>
 
