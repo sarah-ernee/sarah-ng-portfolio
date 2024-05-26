918ae10ce4c2066d57cb349acdfca7705a60b908
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-05-26 13:11:39 +0800

    increased typing time

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -45,7 +45,7 @@ export const Banner = () => {
     } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
-      setDelta(500);
+      setDelta(200);
     }
   };
 
@@ -56,14 +56,13 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm Sarah `}
+              {`Hi I'm Sarah, `}
               <span className="wrap">{text}</span>
             </h1>
             <p>
               After graduating from Petronas University of Technology, I made
-              the leap from earth science to computer science. My interesting
-              journey as such has been about endless learning and discovery.{" "}
-              <br />
+              the leap from earth science to computer science. My journey since
+              has been about endless learning and discovery. <br />
               <br />
               This translates into my pursuit of new tech and writing code that
               doesn't just work, but works brilliantly.
