25900882722b7454feaeb642cca8fc76a3b05063
Sarah Ng <93563644+sarah-ernee@users.noreply.github.com>
2024-05-30 21:14:24 +0800

    changed social icons and added logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Tooltip } from "react-tooltip";
 
 import logo from "../assets/img/logo.svg";
+import { HiOutlineRocketLaunch } from "react-icons/hi2";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -30,8 +32,15 @@ export const NavBar = () => {
   return (
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" />
+        <Navbar.Brand>
+          <div
+            className="d-flex"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            <img src={logo} alt="Logo" className="mr-10" />
+
+            <h5 className="logo-text">SARAH NG</h5>
+          </div>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -72,30 +81,57 @@ export const NavBar = () => {
           <span className="navbar-text">
             <div className="social-icon">
               <a
-                href="https://www.linkedin.com/in/sarah-ng-ern-ee-8b636823a/"
+                href="https://drive.google.com/file/d/1OLvJ4drZdXR-4Ri9d7TOybA_4LPbyLke/view?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
+                data-tooltip-id="resumeTooltip"
+                data-tooltip-content="View CV"
               >
-                <img src={navIcon1} alt="" />
+                <img src={navIcon1} alt="cv" />
               </a>
               <a
                 href="https://www.linkedin.com/in/sarah-ng-ern-ee-8b636823a/"
                 target="_blank"
                 rel="noreferrer"
+                data-tooltip-id="linkedinTooltip"
+                data-tooltip-content="LinkedIn"
               >
-                <img src={navIcon2} alt="" />
+                <img src={navIcon2} alt="linkedin" />
               </a>
               <a
-                href="https://www.linkedin.com/in/sarah-ng-ern-ee-8b636823a/"
+                href="https://github.com/sarah-ernee"
                 target="_blank"
                 rel="noreferrer"
+                data-tooltip-id="githubTooltip"
+                data-tooltip-content="GitHub"
               >
-                <img src={navIcon3} alt="" />
+                <img src={navIcon3} alt="github" />
               </a>
             </div>
           </span>
         </Navbar.Collapse>
       </Container>
+      <Tooltip
+        id="resumeTooltip"
+        place="top"
+        effect="solid"
+        backgroundColor="#333"
+        className="tooltip"
+      />
+      <Tooltip
+        id="linkedinTooltip"
+        place="top"
+        effect="solid"
+        backgroundColor="#333"
+        className="tooltip"
+      />
+      <Tooltip
+        id="githubTooltip"
+        place="top"
+        effect="solid"
+        backgroundColor="#333"
+        className="tooltip"
+      />
     </Navbar>
   );
 };
